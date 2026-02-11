4a246c39e262a48e638198621078a9ba2a21a3c5
Squ1reX <maksimhripinkov658@gmail.com>
2026-02-11 19:58:11 +0300

    feat: Add error message logging

--- src/presentation/api/v1/exceptions.py
+++ src/presentation/api/v1/exceptions.py
@@ -1,8 +1,10 @@
 from fastapi import HTTPException, status
+from src.logger import logger
 
 class NoAccess(HTTPException):
     """Нет права доступа (403)"""
     def __init__(self, message: str = "Доступ запрещен"):
+        logger.info(f"Ошибка 403: {message}")
         super().__init__(
             status_code=status.HTTP_403_FORBIDDEN,
             detail=message
@@ -11,6 +13,7 @@ class NoAccess(HTTPException):
 class NotFound(HTTPException):
     """Данные не найдены (404)"""
     def __init__(self, message: str = "Не найдено"):
+        logger.info(f"Ошибка 404: {message}")
         super().__init__(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=message
@@ -19,6 +22,7 @@ class NotFound(HTTPException):
 class NotCorrect(HTTPException):
     """Не корректный запрос (400)"""
     def __init__(self, message: str = "Некорректный запрос"):
+        logger.info(f"Ошибка 400: {message}")
         super().__init__(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=message
@@ -27,6 +31,7 @@ class NotCorrect(HTTPException):
 class ConflictError(HTTPException):
     """Конфликт данных (409)"""
     def __init__(self, message: str = "Конфликт данных"):
+        logger.info(f"Ошибка 409: {message}")
         super().__init__(
             status_code=status.HTTP_409_CONFLICT,
             detail=message
@@ -35,6 +40,7 @@ class ConflictError(HTTPException):
 class Unauthorized(HTTPException):
     """Требуется авторизация (401)"""
     def __init__(self, message: str = "Требуется авторазиция"):
+        logger.info(f"Ошибка 401: {message}")
         super().__init__(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail=message
