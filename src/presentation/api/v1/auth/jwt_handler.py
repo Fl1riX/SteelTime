089876733ec10f334278cbdcb9fc642fbf35a001
Squ1reX <maksimhripinkov658@gmail.com>
2026-04-11 15:42:01 +0300

    refactor(jwt): Remove unnecessary checks in the JWT token decoding function and create correct type handling

--- src/presentation/api/v1/auth/jwt_handler.py
+++ src/presentation/api/v1/auth/jwt_handler.py
@@ -9,7 +9,7 @@ pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 def hash_password(password: str) -> str:
     """Хэшируем пароль"""
-    return pwd_context.hash(password)
+    return pwd_context.hash(password) # type: ignore
 
 def verify_password(password: str, hashed_password: str) -> bool:
     """Проверяем пароль"""
@@ -20,10 +20,12 @@ def create_access_token(data: dict) -> str:
     logger.info("Создание JWT токена...")
     to_encode = data.copy() # копируем данные, чтобы не менять исходный словарь
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES) # через сколько токен становится не действительным 
-    to_encode.update({
-        "exp": expire,
-        "iat": datetime.utcnow()
-    }) # добавляем чремя через которе истечет действие токена
+    to_encode.update(
+        {
+            "exp": expire,
+            "iat": datetime.utcnow()
+        }
+    ) # добавляем время через которе истечет действие токена
     logger.debug(f"Кодирование информации в токен: {to_encode}...")
     
     if "sub" in to_encode:
@@ -37,32 +39,30 @@ def decode_token(token: str) -> dict | None:
     """Декодировать JWT токен"""
     try:
         logger.info("Декодирование токена...")
-        payload = jwt.decode(token, 
-                             SECRET_KEY, 
-                             algorithms=[ALGORITHM], 
-                             options={
-                                 "require_exp": True,      # проверяем сровк действия токена
-                                 "verify_signature": True  # проверяем подпись
-                             }
-                            ) # декодируем токен
+        payload = jwt.decode(
+            token, 
+            SECRET_KEY, 
+            algorithms=[ALGORITHM], 
+            options={
+                "require_exp": True,      # проверяем сровк действия токена
+                "verify_signature": True  # проверяем подпись
+            }
+        ) # декодируем токен
+        
         logger.debug(f"Декодированная информация: {payload}")
         sub_value = payload.get("sub") # извлекаем данные о пользователе "sub" = subject
-        if sub_value:
+        
+        if sub_value is None:
+            logger.warning("Токен не содержит sub")
+            return None
+        
+        try:
             sub_value = int(sub_value)
+        except (ValueError, TypeError):
+            logger.warning(f"sub_value имеет не корректный: {sub_value}")
         
-        if sub_value is None or not isinstance(sub_value, int):
-            logger.error("Неверный субъект")
-            return None
-        
-        user_id: int = sub_value
-        logger.info(f"Полученный user_id: {user_id}")
-        
-        # проверяем что пользователь существует
-        if user_id is None:
-            logger.warning("Токен без user_id")
-            return None
-        
-        return {"sub": user_id}
+        logger.info(f"Полученный user_id: {sub_value}")
+        return {"sub": sub_value}
     except JWTError as e:
         logger.warning(f"Невалидный токен: {e}")
         return None
