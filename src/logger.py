632d9e9f356947c2290e800ea800467669b341ce
Squ1reX <82816845+Fl1riX@users.noreply.github.com>
2026-05-30 16:05:35 +0300

    Revert "Feature/docker security improvements (#25)"
    This reverts commit e57842374bd2ec1795633435bd3a2d7eef2cfa02.

--- src/logger.py
+++ src/logger.py
@@ -9,4 +9,8 @@ formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s') # з�
 console_handler = logging.StreamHandler() # вывод логов в консоль
 console_handler.setFormatter(formatter)
 
+handler = logging.FileHandler(filename="SteelTime.log", encoding="utf-8") # файл для записи логов
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
 logger.addHandler(console_handler)
