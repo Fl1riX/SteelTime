d4e63176b427bb9c8434c58634c00509705f562f
Squ1reX <maksimhripinkov658@gmail.com>
2026-05-31 17:34:38 +0300

    fix: Remove the backup key from config.py

--- src/config.py
+++ src/config.py
@@ -8,6 +8,6 @@ BOT_SECRET = os.getenv("BOT_SECRET")
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-SECRET_KEY = os.getenv("SECRET_KEY", "development-secret-key-i-love_coding")
+SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
