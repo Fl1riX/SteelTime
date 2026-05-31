d9e270b309259c5321750d1e8aed7028c88a979d
Squ1reX <maksimhripinkov658@gmail.com>
2026-05-31 18:09:05 +0300

    fix(auth): Fixing a missing SECRET_KEY error in .env

--- src/config.py
+++ src/config.py
@@ -6,8 +6,13 @@ load_dotenv()
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 BOT_SECRET = os.getenv("BOT_SECRET")
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.environ["DATABASE_URL"]
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+def get_secret_key() -> str:
+    secret = os.getenv("SECRET_KEY")
+    if not secret:
+        raise RuntimeError("!!! Не задан секретный ключ !!!")
+    return secret
+    
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
