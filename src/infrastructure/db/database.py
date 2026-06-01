422bacc731631e477b5d57f50a813a99945f1696
Squ1reX <maksimhripinkov658@gmail.com>
2026-06-01 10:44:58 +0300

    Add a database engine to lifespan's fastapi

--- src/infrastructure/db/database.py
+++ src/infrastructure/db/database.py
@@ -1,23 +1,18 @@
 #from pathlib import Path
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
 from sqlalchemy.orm import declarative_base
-from src.config import DATABASE_URL
-
-DB_URL = f"postgresql+asyncpg://{DATABASE_URL}"
-
-engine = create_async_engine(
-    DB_URL
-)
-
-SessionLocal = async_sessionmaker(
-    bind=engine, 
-    class_=AsyncSession, 
-    expire_on_commit=False
-)
+from fastapi import Request
 
 Base = declarative_base()
 
-async def get_db():
+async def get_db(request: Request):
+    
+    SessionLocal = async_sessionmaker(
+        bind=request.app.state.engine, 
+        class_=AsyncSession, 
+        expire_on_commit=False
+    )
+    
     async with SessionLocal() as session:
         try:
             yield session
