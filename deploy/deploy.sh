4053750fd8a859becc17cd5560a9982786309483
Squ1reX <maksimhripinkov658@gmail.com>
2026-08-16 15:40:06 +0300

    feat(deploy): improve the process of applying changes to the server

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,9 +6,21 @@ cd /home/fl1rix/SteelTime/
 
 echo "----- Deploy started: $(date) -----" >> ./deploy.log
 
-git pull origin main >> ./deploy.log 2>&1
+# Скачаиваем изменения
+git fetch origin main >> ./deploy.log 2>&1
+
+# Стираем любые незакоммиченные изменения
+git checkout . >> ./deploy.log 2>&1
+
+# Жестко приравниваем локальную ветку к удаленной
+git checkout . >> ./deploy.log 2>&1
+
+# Удаляем все неотслеживаемые файлы
+git clean -fd >> ./deploy.log 2>&1
+
 git status >> ./deploy.log 2>&1
 
+# Обновляем и перезапускаем Docker
 docker compose pull >> ./deploy.log 2>&1
 docker compose up -d >> ./deploy.log 2>&1
 
