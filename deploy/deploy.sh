ebc1412493f6a89fa3dde03ae5461e9e26ca14ed
Squ1reX <82816845+Fl1riX@users.noreply.github.com>
2026-08-16 15:44:34 +0300

    Fix/webhook deploy (#44)
    * fix(migrations):Fix the initial schema by recreating it to match the current database models.
    
    * fix(migrations): remove duplicate inital migration with scale enum approach
    
    * fix(deploy): fix indentation in the webhook config
    
    * feat(deploy): improve logging of the deployment execution script
    
    * fix(deploy): remove set -x
    
    * feat(deploy): improve the process of applying changes to the server
    
    * refactor(deploy): move the logging to the beginning of the script
    
    ---------
    
    Co-authored-by: Squ1reX <maksimhripinkov658@gmail.com>

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
 set -e
-
 cd /home/fl1rix/SteelTime/
 
-git pull origin main
+# Включаем автоматическое логирование ВСЕГО скрипта в файл
+exec >> ./deploy.log 2>&1
+
+echo "----- Deploy started: $(date) -----"
+
+# Скачиваем изменения из репозитория
+git fetch origin main
+
+# Стираем любые изменения в текущих файлах
+git checkout .
+
+# Жестко двигаем ветку вперед до актуального состояния на GitHub/GitLab
+git reset --hard origin/main
+
+# Удаляем весь случайный мусор и новые неотслеживаемые файлы
+git clean -fd
+
+git status
+
+# Обновляем и перезапускаем Docker
 docker compose pull
 docker compose up -d
+
+echo "--- Deploy finished successfully ---"
