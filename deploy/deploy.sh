1f217d332aa79d6ffe5768f628762c75c465fd66
Squ1reX <maksimhripinkov658@gmail.com>
2026-08-16 15:28:52 +0300

    feat(deploy): improve logging of the deployment execution script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 cd /home/fl1rix/SteelTime/
 
-git pull origin main
-docker compose pull
-docker compose up -d
+echo "----- Deploy started: $(date) -----" >> ./deploy.log 2>&1
+
+git pull origin main >> ./deploy.log 2>&1
+git status >> ./deploy.log 2>&1
+
+docker compose pull >> ./deploy.log 2>&1
+docker compose up -d >> ./deploy.log 2>&1
+
+echo "--- Deploy finished successfully ---" >> ./deploy.log 2>&1
