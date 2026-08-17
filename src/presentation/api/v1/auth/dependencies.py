930f2276428227cd9635dab7b6f7ae3933c3f705
Squ1reX <maksimhripinkov658@gmail.com>
2026-08-17 17:26:08 +0300

    feat(auth): add a check for user bans

--- src/presentation/api/v1/auth/dependencies.py
+++ src/presentation/api/v1/auth/dependencies.py
@@ -5,6 +5,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.infrastructure.db.database import get_db
 from src.presentation.api.v1.auth.jwt_handler import decode_token
 from src.domain.services.user_service import UserService
+from src.domain.models.user_model import User
+from src.presentation.api.v1.exceptions import NoAccess
 from src.logger import logger
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login") # указываем URL эндпоинта логина
@@ -71,8 +73,24 @@ async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession
     
     return user
         
+async def get_active_user(
+    current_user: User = Depends(get_current_user),
+) -> User:
+    """
+    Проверяем что пользователь не забанен 
+    и возвращаем объект пользователя, 
+    если он не забанен или ошибку 403
+    """
     
-    
+    for ban in current_user.bans: # current_user.bans вернет список объектов(банов)
+        if ban.is_active: # Проходимя по списку и если если бан активен, то возвращаем ошибку
+            raise NoAccess(
+                f"Вы были забанены.\n \
+                Причина: {ban.reason}\n \
+                Срок истечения: {ban.expires_at}"
+            )
+        
+    return current_user
     
     
     
